7bb951d9e8273d70d4eb94b5a86f46df34a1ef38
collin <collin.busschots@capgemini.com>
2022-12-03 15:27:49 +0100

    Add assertion for challenge three

--- challenges/three_test.go
+++ challenges/three_test.go
@@ -6,9 +6,13 @@ import (
 )
 
 func TestChallengeThree(t *testing.T) {
+	expectedPriorities := 7727
 	input := readInputFromFile("input-three")
 
 	priorities := ChallengeThree(input)
-
 	fmt.Println(priorities)
+
+	if priorities != expectedPriorities {
+		t.Fatalf("%d is not the right sum of priorities", priorities)
+	}
 }
