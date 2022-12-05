ea2b81d206b5b4aa73a914b11a7e2d668af6b686
collin <collin.busschots4@gmail.com>
2022-12-05 17:50:48 +0100

    Solve challenge four part two

--- challenges/four.go
+++ challenges/four.go
@@ -13,26 +13,57 @@ type Schedule struct {
 func challengeFour(input []string) int {
 	fullyContained := 0
 	for _, schedule := range input {
-		cleanAreas := strings.Split(schedule, ",")
-		firstStringSchedule := strings.Split(cleanAreas[0], "-")
-		start, _ := strconv.Atoi(firstStringSchedule[0])
-		end, _ := strconv.Atoi(firstStringSchedule[1])
-		firstSchedule := Schedule{
-			start: start,
-			end:   end,
-		}
-		secondStringSchedule := strings.Split(cleanAreas[1], "-")
-		start, _ = strconv.Atoi(secondStringSchedule[0])
-		end, _ = strconv.Atoi(secondStringSchedule[1])
-		secondSchedule := Schedule{
-			start: start,
-			end:   end,
-		}
+		firstSchedule, secondSchedule := setupSchedules(schedule)
 		if isFullyContained(firstSchedule, secondSchedule) {
 			fullyContained += 1
 		}
 	}
 	return fullyContained
+}
+
+func challengeFourPartTwo(input []string) int {
+	partiallyContained := 0
+	for _, schedule := range input {
+		firstSchedule, secondSchedule := setupSchedules(schedule)
+		if isPartiallyContained(firstSchedule, secondSchedule) {
+			partiallyContained += 1
+		}
+	}
+	return partiallyContained
+}
+
+func setupSchedules(schedule string) (Schedule, Schedule) {
+	cleanAreas := strings.Split(schedule, ",")
+	firstStringSchedule := strings.Split(cleanAreas[0], "-")
+	start, _ := strconv.Atoi(firstStringSchedule[0])
+	end, _ := strconv.Atoi(firstStringSchedule[1])
+	firstSchedule := Schedule{
+		start: start,
+		end:   end,
+	}
+	secondStringSchedule := strings.Split(cleanAreas[1], "-")
+	start, _ = strconv.Atoi(secondStringSchedule[0])
+	end, _ = strconv.Atoi(secondStringSchedule[1])
+	secondSchedule := Schedule{
+		start: start,
+		end:   end,
+	}
+	return firstSchedule, secondSchedule
+}
+
+func isPartiallyContained(firstSchedule Schedule, secondSchedule Schedule) bool {
+	lowerBounds := min(firstSchedule.start, secondSchedule.start)
+	if firstSchedule.start == lowerBounds {
+		if firstSchedule.end >= secondSchedule.start || firstSchedule.end >= secondSchedule.end {
+			return true
+		}
+	}
+	if secondSchedule.start == lowerBounds {
+		if secondSchedule.start <= firstSchedule.start && secondSchedule.end >= firstSchedule.start {
+			return true
+		}
+	}
+	return false
 }
 
 func isFullyContained(firstSchedule Schedule, secondSchedule Schedule) bool {
--- challenges/four_test.go
+++ challenges/four_test.go
@@ -32,3 +32,30 @@ func TestChallengeFourExtraInput(t *testing.T) {
 		t.Fatalf("%d is not the right amount of pairs", solelyContainedPairs)
 	}
 }
+
+func TestChallengeFourPartTwo(t *testing.T) {
+	input := readInputFromFile("input-four")
+
+	partiallyContainedPairs := challengeFourPartTwo(input)
+	fmt.Println(partiallyContainedPairs)
+}
+
+func TestChallengeFourPartTwoExtraInput(t *testing.T) {
+	expectedPairs := 4
+	input := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8",
+		"75-75,72-74",
+	}
+
+	partiallyContainedPairs := challengeFourPartTwo(input)
+	fmt.Println(partiallyContainedPairs)
+
+	if partiallyContainedPairs != expectedPairs {
+		t.Fatalf("%d is not the right amount of pairs", partiallyContainedPairs)
+	}
+}
